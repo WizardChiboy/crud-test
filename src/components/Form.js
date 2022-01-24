16fd744ffa9e72dba13e367327ea7c68671a714a
wizard chiboy <doncodesacademy@gmail.com>
2022-01-24 13:41:46 +0100

    done

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Form.css";
 import axios from "../axios";
 
 function Form() {
+  const [title, setTitle] = useState("");
+  const [excerpt, setExcerpt] = useState("");
+  const [content, setContent] = useState("");
+
+  const postSubmit = async (e) => {
+    e.preventDefault();
+    console.log("form submitted");
+
+    axios.post("/posts", {
+      title: title,
+      excerpt: excerpt,
+      content: content,
+    });
+
+    setTitle("");
+    setExcerpt("");
+    setContent("");
+  };
   return (
     <>
       <div className="form">
@@ -11,12 +29,29 @@ function Form() {
         </div>
 
         <div className="form-content">
-          <form action="/posts" method="post">
+          <form onSubmit={postSubmit}>
             <h3>Title</h3>
-            <input type="text" name="title" id="" />
-            <h3>Excerpt</h3> <input type="text" name="excerpt" id="" />
+            <input
+              type="text"
+              name="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+            />
+            <h3>Excerpt</h3>
+            <input
+              type="text"
+              name="excerpt"
+              value={excerpt}
+              onChange={(e) => setExcerpt(e.target.value)}
+            />
             <h3>Content</h3>
-            <textarea name="" id="" cols="30" rows="10"></textarea>
+            <textarea
+              name="content"
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+              cols="30"
+              rows="10"
+            ></textarea>
             <button type="submit"> Post </button>
           </form>
         </div>
